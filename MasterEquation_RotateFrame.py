3e06900497710efc3984a499cb7b506f3ec8e65c
Victor Teng <xassam@mail.ustc.edu.cn>
2024-03-21 10:48:44 +0800

    加入拟合 改为plot矩阵元
    验证了T2 = 2T1极限是应用于相干矩阵元的；实验上是分别测0态的投影和1态的投影，相减就是2 * Re(c)，所以应该plot矩阵元

--- MasterEquation_RotateFrame.py
+++ MasterEquation_RotateFrame.py
@@ -2,11 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from qutip import *
+from scipy.optimize import curve_fit
 
 # 系统参数
 levels = 3  # 能级数
 S = (levels-1)/2  # 自旋
-gamma = np.array([[0,98,130],[98,0,100],[130,100,0]])  # 退极化率
+gamma = np.array([[0,100,130],[100,0,100],[130,100,0]])  # 退极化率
 
 # 定义哈密顿量，旋转坐标系下Lindblad方程不含H，所以设为零
 H = qzero(levels)
@@ -19,9 +20,8 @@ HalfPi_Sy10 = (-1j * 0.5 * np.pi * Sy10).expm() # Sy的pi/2脉冲
 # 定义初始状态（假设实验操控+1和0能级）
 psi0 = Qobj([[1],[1],[0]]) / np.sqrt(2)  # 初态为0，+1的两能级+x态
 
-
 # 定义时间点
-tlist = np.linspace(0, 10 ** (-3), 10000) 
+tlist = np.linspace(0, 35 * 10 ** (-3), 100000) 
 
 
 # 定义Lindblad算符
@@ -37,19 +37,41 @@ result = mesolve(H, psi0, tlist, L, [])
 
 
 # 施加half-pi脉冲，并计算0态概率
-Coherence = []
-Proj = basis(3,1) * basis(3,1).dag()
+# Coherence = []
+# Proj = basis(3,1) * basis(3,1).dag()
+# for n in range(len(tlist)):
+#     density = result.states[n]
+#     density_Rot = HalfPi_Sy10 * density * HalfPi_Sy10.dag()
+#     Coherence.append((density_Rot * Proj).tr())
+Coherence10 = []
+Coherence1_1 = []
+Coherence0_1 = []
 for n in range(len(tlist)):
     density = result.states[n]
-    density_Rot = HalfPi_Sy10 * density * HalfPi_Sy10.dag()
-    Coherence.append((density_Rot * Proj).tr())
-
+    Coherence10.append(2 * np.real(density[0,1]))
+    Coherence1_1.append(2 * np.real(density[0,2]))
+    Coherence0_1.append(2 * np.real(density[1,2]))
                
 
 # 绘制0能级占据概率随时间的变化
 plt.figure(figsize=(8, 5))
-plt.plot(tlist, Coherence, label=f"Level")
+plt.plot(tlist, Coherence10, label=f"10")
+plt.plot(tlist, Coherence1_1, label=f"1-1")
+plt.plot(tlist, Coherence0_1, label=f"0-1")
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
 plt.show()
+
+#拟合T2
+def func(t, T2):
+    return np.exp(- t / T2)
+
+T1 = 1 / (2 * gamma[0,1] + gamma[0,2] + gamma[1,2])
+
+popt, pcov = curve_fit(func, tlist, Coherence10)
+plt.scatter(tlist, Coherence10, label='Data')
+plt.plot(tlist, func(tlist, *popt), 'r-', label= f"T2 = {popt} 2T1 = {2 * T1}")
+plt.legend()
+plt.show()
+
